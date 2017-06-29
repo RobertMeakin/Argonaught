046bed7652223981e9204a8605b0db5abab1e273
Robert Meakin <meakin.robert@gmail.com>
2017-06-29 12:55:03 +0100

    fixed form key check error with guard clause

--- Internal/RequestTypes/RequestFormat/UnknownRequestFormat.cs
+++ Internal/RequestTypes/RequestFormat/UnknownRequestFormat.cs
@@ -8,8 +8,12 @@ namespace Argonaught.Internal.RequestTypes.RequestFormat
         public string FindErrors(HttpContext context, ArgonautOptions options)
         {
             //Check to see if the user is attemping to request an access token by checking if the 'grant_type' parameter exists, even though the request type could not be found.
+            if (!context.Request.HasFormContentType)
+                return "";
+            
             if (context.Request.Form.ContainsKey("grant_type"))
                 return "The request is not properly formatted.";
+            //End check
             
             return "";
         }
