67e09551cf67446742e236b7cf0863c75adadefe
Mac <mac@Macs-MacBook-Pro.local>
2018-07-09 12:19:54 +0100

    removed need for authorization field when hitting url with /api

--- Internal/RequestTypes/RequestFormat/BearerRequestFormat.cs
+++ Internal/RequestTypes/RequestFormat/BearerRequestFormat.cs
@@ -1,17 +1,14 @@
 using Argonaught.Internal.RequestTypes.RequestFormat.Interfaces;
 using Microsoft.AspNetCore.Http;
 
-namespace Argonaught.Internal.RequestTypes.RequestFormat
-{
-    internal class BearerRequestFormat : IRequestTypeFormat
-    {
-        public string FindErrors(HttpContext context, ArgonautOptions options)
-        {
-            if (!context.Request.Headers.ContainsKey("authorization"))
-                return "An authorization header is required.";
+namespace Argonaught.Internal.RequestTypes.RequestFormat {
+    internal class BearerRequestFormat : IRequestTypeFormat {
+        public string FindErrors(HttpContext context, ArgonautOptions options) {
 
-            if (!context.Request.Headers["authorization"].ToString().Contains("Bearer "))
+            if (context.Request.Headers.ContainsKey("authorization") &&
+                !context.Request.Headers["authorization"].ToString().Contains("Bearer ")) {
                 return "The authorization field needs to be formatted as Bearer [access_token].";
+            }
 
             return "";
         }
